462d584c57c2ad2653e3449259254bb544258d5c
Tim Cashmore <tim.cashmore@sap.com>
2023-10-20 17:37:33 +0000

    Version 0.0.5

--- webapp/controller/req.controller.js
+++ webapp/controller/req.controller.js
@@ -2,64 +2,58 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
     "sap/ui/table/library",
-    "sap/m/DynamicDateUtil",
-    "sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
+    'sap/m/DynamicDateRange',
+    "sap/ui/model/odata/ODataUtils",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel,library,DynamicDateUtil,Filter, FilterOperator) {
+    function (Controller,JSONModel,library, DynamicDateRange,ODataUtils) {
         "use strict";
-
+        
         return Controller.extend("tc.requisitions.controller.req", {
             onInit: function () {
-            var oView = this.getView();
-            //Upload the model from API call or local test data
-//			oView.setModel(this.initDataModel(),"reqs");
-            oView.setModel(this.initDataModel());
-            // Initial Sorting
-            var oReqID = this.oView.byId("reqNum");
-			oView.byId("table").sort(oReqID, library.SortOrder.Ascending);
 
+                var oStandardOptions = ["DATE", "TODAY", "YESTERDAY", "FIRSTDAYWEEK", "FIRSTDAYMONTH", "FIRSTDAYQUARTER", 
+                "FIRSTDAYYEAR", "DATERANGE", "DATETIMERANGE", "YEARTODATE", "LASTMINUTES", "LASTHOURS", "LASTDAYS", 
+                "LASTWEEKS", "LASTMONTHS", "LASTQUARTERS", "LASTYEARS", "TODAYFROMTO", "THISWEEK", "LASTWEEK", "SPECIFICMONTH", "SPECIFICMONTHINYEAR", 
+                "THISMONTH", "LASTMONTH", "THISQUARTER", "LASTQUARTER", "QUARTER1", "QUARTER2", "QUARTER3", "QUARTER4", "THISYEAR", "LASTYEAR"];
+                var oDynamicRange = this.getView().byId("dynamic-range");
+                oDynamicRange.setStandardOptions(oStandardOptions);
             },
-            initDataModel : function() {
-                console.log("in initDataModel");
+            onChange: function(oEvent) {
+                var oValue = oEvent.getParameter("value");
+                var bValid = oEvent.getParameter("valid");
+                var aDates = DynamicDateRange.toDates(oValue);
+                console.log("aDates = "+aDates.length+" "+aDates[0]+" "+aDates[1]);
+                if (oValue != null && bValid) {
+                    // Reload data
+                    this.loadDataModel(aDates[0],aDates[1]);		                
+                }             
+            }, 
+            loadDataModel: function(fromDate, toDate) {
+                console.log("hostname = "+window.location.hostname);
+//                var URI =  "/PurchaseRequisition?$top=10000&$filter=Material eq 'MZ-TG-0011' and CreationDate gt "+this.dateAsURIParam(fromDate)+"and CreationDate le "+this.dateAsURIParam(toDate);
+                var URI =  "PurchaseRequisition?$top=10000&$filter=CreationDate gt "+this.dateAsURIParam(fromDate)+"and CreationDate le "+this.dateAsURIParam(toDate);
+                console.log("URI= "+URI);
                 var oReqs = new JSONModel();
-                // Local Test requires a prefix '/' whereas using the destination need to remove the '/' ??
-                // Version to use when deployed to BTP using a Destination
-//                var URI = "PurchaseRequisition?$top=10000&$filter=CreationDate gt datetime'2023-03-21T00:00:00.000' and Material eq 'MZ-TG-0011'"
-                var URI = "/PurchaseRequisition?$top=10000&$filter=CreationDate gt datetime'2023-03-21T00:00:00.000' and Material eq 'MZ-TG-0011'"
-//                var URI = "../testData/requisitionListSampleData.json";   // Local Test Data
                 var oPromise = oReqs.loadData(URI);
                 oPromise.then( () => {
                     //How many items
                     var oLen = oReqs.getData().length;
-                    console.log("length = "+oLen);                    
-//                    console.log("JSON Model = "+oReqs.getJSON());
+                    console.log("reload length = "+oLen);                    
                     var oUIModel = new JSONModel({
                         numReqs: oLen
                     });
-                    this.getView().setModel(oUIModel,"ui");
-               });
-               return oReqs;
+                    var oView = this.getView();
+                    oView.setModel(oUIModel,"ui");
+                    oView.setModel(oReqs,"reqs");
+                    var oReqID = oView.byId("reqNum");
+                    oView.byId("reqtable").sort(oView.byId("reqNum"), library.SortOrder.Descending);
+                }) 
             },
-            onDynamicDateChange: function(oEvent) {
-                var oValue = oEvent.getParameter("value");
-                // console.log("on change check event value "+ oValue);
-                if (oValue != null) {
-                    var aDates = DynamicDateUtil.toDates(oValue);
-                    var fromDate = (new Date(aDates[0])).toISOString();
-                    var toDate = (new Date(aDates[1])).toISOString();
-                    console.log("fromX="+ fromDate);
-                    console.log("toX="+ toDate);		                
-            }
-            
- 
-            },
-            loadDataModel: function(fromDate, toDate) {
-
-
+            dateAsURIParam: function (vValue) {
+                return vValue ? ODataUtils.formatValue(vValue, "Edm.DateTime") : "<null>";
             }
 
         });
